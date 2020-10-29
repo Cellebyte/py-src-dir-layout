8bd2013adb7c0d8a774c4ee80756b6612696639a
Cellebyte <marcel.fest@live.de>
2020-10-29 10:12:38 +0100

    Added version

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-  name="rocket-science",
+    name="rocket-science",
+    version="0.0.1",
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
 )
